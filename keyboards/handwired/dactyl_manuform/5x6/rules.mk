2ae168015497278daa2379a5838c174dc8db4167
Andreas Mager <andreas@mager.eu>
2022-05-01 22:17:46 +0200

    neo layer 1 added mit unicode support

--- keyboards/handwired/dactyl_manuform/5x6/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x6/rules.mk
@@ -17,3 +17,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
+
+DEBOUNCE_TYPE=sym_eager_pk
+UNICODEMAP_ENABLE = yes
