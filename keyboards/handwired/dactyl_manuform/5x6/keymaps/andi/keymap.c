a8e0cb7919cab793ef2fc72f41860dbf2afa26f3
Andreas Mager <andreas@mager.eu>
2022-05-06 23:11:05 +0200

    default layout changed

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/andi/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/andi/keymap.c
@@ -1,12 +1,15 @@
 #include QMK_KEYBOARD_H
 //#include "keymap_german.h"
 
-#define _NEO_1 0
-#define _LOWER 1
-#define _RAISE 2
-#define _NEO_3 3
-#define _NEO_4 4
-#define _NEO_D 5
+#define _NEO_1 1
+#define _LOWER 2
+#define _RAISE 3
+#define _NEO_3 4
+#define _NEO_4 5
+#define _NEO_D 6
+#define _MAG   0
+#define _DIA   7
+
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
@@ -58,9 +61,15 @@ const uint32_t PROGMEM unicode_map[] = {
 #define MLALT LALT_T(KC_LBRC)
 #define MRALT LALT_T(KC_RBRC)
 
-#define SNEO4 LT(_NEO_4, KC_SPC)
-#define ENEO4 LT(_NEO_4, KC_ENT)
+#define SNEO3 LT(_NEO_3, KC_SPC)
+#define ENEO3 LT(_NEO_3, KC_ENT)
 
+#define SNEO4 LT(_NEO_4, KC_ESC)
+#define ENEO4 LT(_NEO_4, KC_ESC)
+
+#define MLALT2 OSL(_DIA)
+
+#define MRALT2 LALT_T(KC_SLSH)
 /*
  *
  *
@@ -91,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LBRC, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
         KC_LALT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_LALT,
                          KC_BSPC,KC_DEL,                                                        KC_ESC, NEO_3,
-                                         KC_LSFT,KC_ENT,                         KC_SPC ,KC_RSFT,
+                                           KC_LSFT,KC_ENT,                         KC_SPC ,KC_RSFT,
                                          KC_LGUI,NEO_4 ,                         NEO_4  ,KC_RGUI,
                                          KC_LCTL,KC_CAPS,                        KC_CAPS,KC_RCTL
     ),KC_QUOT,
@@ -100,19 +109,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9 , KC_0 ,KC_EQL ,
         KC_TAB , KC_X  , KC_V  , KC_L  , KC_C  , KC_W  ,                         KC_K  , KC_H  , KC_G  , KC_F , KC_Q ,KC_SCLN,
         KC_ESC , KC_U  , KC_I  , KC_A  , KC_E  , KC_O  ,                         KC_S  , KC_N  , KC_R  , KC_T , KC_D ,KC_SLSH,
-        MLALT  ,KC_QUOT,KC_Y   ,NEO_DIA, KC_P  , KC_Z  ,                         KC_B  , KC_M  ,KC_COMM,KC_DOT, KC_J ,MRALT  ,
-                         _______,_______,                                                    _______, RAISE,
-                                         KC_LSFT,ENEO4  ,                        SNEO4  ,KC_RSFT,
-                                         KC_LGUI,NEO_3  ,                        NEO_3  ,KC_RGUI,
+        MLALT  ,KC_QUOT,LALT_T(KC_Y),LGUI_T(NEO_DIA),LCTL_T(KC_P), KC_Z  ,                         KC_B  ,RCTL_T(KC_M),RGUI_T(KC_COMM),LALT_T(KC_DOT), KC_J ,MRALT  ,
+                         DF(_MAG),_______,                                                    _______, RAISE,
+                                         KC_LSFT,ENEO3  ,                        SNEO3  ,KC_RSFT,
+                                         KC_LGUI,ENEO4  ,                        SNEO4  ,KC_RGUI,
                                          KC_BSPC,KC_LCTL,                        KC_RCTL,KC_DEL
     ),
 
+
+    [_MAG] = LAYOUT_5x6(
+        KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9 , KC_0 ,KC_EQL ,
+        KC_TAB , KC_V  , KC_P  , KC_L  , KC_C  , KC_F  ,                         KC_K  , KC_U  , KC_O  , KC_Y , KC_J ,KC_SCLN,
+        KC_ESC , KC_R  , KC_T  , KC_N  , KC_S  , KC_D  ,                        KC_QUOT, KC_A  , KC_E  , KC_I , KC_H ,KC_BSPC,
+        MLALT2 , KC_Z  ,LALT_T(KC_B),LGUI_T(KC_M),LCTL_T(KC_G), KC_W  ,                         KC_X  ,RCTL_T(KC_COMM),RGUI_T(KC_DOT),LALT_T(KC_MINS),KC_Q ,MRALT2 ,
+                         DF(_NEO_1),_______,                                                    _______, RAISE,
+                                         KC_LSFT,ENEO3  ,                        SNEO3  ,KC_RSFT,
+                                         KC_LGUI,ENEO4  ,                        SNEO4  ,KC_RGUI,
+                                         KC_BSPC,KC_LCTL,                        KC_RCTL,KC_DEL
+    ),
+
+    [_DIA] = LAYOUT_5x6(
+
+        _______,_______,_______,_______,_______,_______,                  _______, _______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,                  _______, DE_UE  , DE_OE ,_______,_______,_______,
+        _______,_______,_______,_______,DE_SS  ,_______,                  _______, DE_AE  ,DE_EURO,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,                  _______, _______,_______,_______,_______,_______,
+                                       _______,_______,                 _______,_______,
+                                           _______,_______,          _______,_______,
+                                           _______,_______,          _______,_______,
+                                           _______,_______,          _______,_______
+
+    ),
+
     [_LOWER] = LAYOUT_5x6(
-        KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
-        _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-        _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                                                _______,KC_PSCR,            _______, KC_P0,
+        _______,_______,_______,_______,_______,_______,                  _______, _______,_______,_______,_______,_______,
+        _______,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                  KC_EQL , KC_P7  , KC_P8 , KC_P9 ,KC_PLUS,_______,
+        _______,KC_PIPE,KC_AMPR,KC_LCBR,KC_LPRN,KC_LBRC,                  KC_ASTR, KC_P4  , KC_P5 , KC_P6 ,KC_TILD,_______,
+        _______,KC_CIRC,KC_BSPC,KC_RCBR,KC_RPRN,KC_RBRC,                  KC_0   , KC_P1  , KC_P2 , KC_P3 ,KC_GRV ,_______,
+                                                _______,_______,            _______,_______,
                                                 _______,_______,            _______,_______,
                                                 _______,_______,            _______,_______,
                                                 _______,_______,            _______,_______
@@ -133,10 +167,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* NEO_4: Cursor & Numpad
    */
   [_NEO_4] = LAYOUT_5x6(
-        _______,_______,_______  ,_______  ,_______ ,_______  ,                  _______, KC_TAB,KC_PSLS,KC_PAST,KC_PMNS,_______,
-        _______,KC_PGUP,KC_BSPACE,KC_UP    ,KC_DEL  ,KC_PGDOWN,                  KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,KC_PLUS,_______,
-        _______,KC_HOME,KC_LEFT  ,KC_DOWN  ,KC_RIGHT,KC_END   ,                  KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_PCMM,KC_KP_DOT,
-        _______,KC_ESC ,KC_TAB   ,KC_INSERT,KC_ENTER,KC_UNDO  ,                  _______, KC_P1 , KC_P2 , KC_P3 ,KC_SCLN,_______,
+        _______,_______,KC_KB_VOLUME_DOWN,KC_KB_VOLUME_UP,_______ ,_______ ,                  _______, _______,KC_PSLS,KC_PAST,KC_PMNS,_______,
+        _______,KC_BSPC,KC_UNDO          ,KC_AGIN        ,KC_DEL  ,_______ ,                  _______, _______ , KC_P8 , KC_P9 ,KC_PLUS,_______,
+        _______,KC_LEFT,KC_DOWN          , KC_UP         ,KC_RIGHT,_______ ,                  _______, _______ , KC_P5 , KC_P6 ,KC_PCMM,KC_KP_DOT,
+        _______,KC_HOME,KC_PGDOWN        , KC_PGUP       ,KC_END  ,_______ ,                  _______, _______ , KC_P2 , KC_P3 ,KC_SCLN,_______,
                                                 _______,KC_PSCR,            _______,_______,
                                                 _______,_______,            KC_0   ,_______,
                                                 _______,_______,            _______,_______,
@@ -169,6 +203,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           )
 /*
  *
+   NEO_4: Cursor & Numpad
+  /
+  [_NEO_4] = LAYOUT_5x6(
+        _______,_______,_______  ,_______ ,_______ ,_______ ,                  _______, KC_TAB,KC_PSLS,KC_PAST,KC_PMNS,_______,
+        _______,KC_BSPC,KC_UNDO  ,KC_AGIN ,KC_DEL  ,_______ ,                  KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,KC_PLUS,_______,
+        _______,KC_LEFT,KC_DOWN  , KC_UP  ,KC_RIGHT,_______ ,                  KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_PCMM,KC_KP_DOT,
+        _______,KC_HOME,KC_PGDOWN,KC_PGUP ,KC_END  ,_______ ,                  _______, KC_P1 , KC_P2 , KC_P3 ,KC_SCLN,_______,
+                                                _______,KC_PSCR,            _______,_______,
+                                                _______,_______,            KC_0   ,_______,
+                                                _______,_______,            _______,_______,
+                                                _______,_______,            _______,_______
+  ),
     [_NEO_3] = LAYOUT_5x6(
 
         _______,_______,_______,_______,_______,_______,                  _______, _______,_______,_______,_______,_______,
