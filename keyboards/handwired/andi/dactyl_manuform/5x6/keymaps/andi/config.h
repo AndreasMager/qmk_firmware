97ffe9bc9a43d4eeffabfb84e3b9096fa05522d6
Andreas Mager <andreas@mager.eu>
2024-07-02 20:52:06 +0200

    old version added to handwired/andi

---
+++ keyboards/handwired/andi/dactyl_manuform/5x6/keymaps/andi/config.h
@@ -0,0 +1,33 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define USE_SERIAL
+
+#define MASTER_LEFT
+//#define MASTER_RIGHT
+
+//#define TAPPING_TERM 250
+
+//#define TAPPING_TOGGLE 3
+//#define UNICODE_SELECTED_MODES UC_LNX
+
+//#define EE_HANDS
+//#define ONESHOT_TAP_TOGGLE 3
+//#define COMBO_COUNT 0
+//#define COMBO_LEN 0
