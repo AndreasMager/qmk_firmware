37333fbef91279d560fb81a5f6c96aa0f1a862a7
Andreas Mager <andreas@mager.eu>
2024-12-27 14:33:49 +0100

    some keys moved:

--- keyboards/handwired/andi/dactyl_manuform_rp/5x6/keymaps/andi/keymap.c
+++ keyboards/handwired/andi/dactyl_manuform_rp/5x6/keymaps/andi/keymap.c
@@ -35,28 +35,17 @@
 #define KC_OCAG OSM(MOD_LCTL|MOD_LALT|MOD_LGUI)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
     [_MAG] = LAYOUT_5x6(
-        KC_CUT , KC_F1 , KC_F2 ,LGUI_T(KC_F3),KC_F4, KC_F5 ,                     KC_F6 , KC_F7 ,RGUI_T(KC_F8),RALT_T(KC_F9),KC_F10,KC_PSCR,
-        KC_CAPS, KC_V  , KC_P  , KC_L  , KC_C  , KC_F  ,                         KC_K  , KC_U  , KC_O  , KC_Y , KC_J ,KC_FIND,
-        KC_PSTE, KC_R  , KC_T  , KC_N  , KC_S  , KC_D  ,                        KC_QUOT, KC_A  , KC_E  , KC_I , KC_H ,KC_F21 ,
-        KC_APP , KC_Z  , KC_B  , KC_M  , KC_G  , KC_W  ,                         KC_X  ,KC_DOT ,KC_MINS,KC_COMM,KC_Q ,_______,
+        KC_DEL, KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                         KC_F6 , KC_F7 , KC_F8 , KC_F9 ,KC_F10,KC_PSCR,
+        KC_AGAIN, KC_V  , KC_P  , KC_L  , KC_C  , KC_F  ,                         KC_K  , KC_U  , KC_O  , KC_Y  , KC_J ,KC_FIND,
+        KC_UNDO, KC_R  , KC_T  , KC_N  , KC_S  , KC_D  ,                        KC_QUOT, KC_A  , KC_E  , KC_I  , KC_H ,KC_CAPS,
+        KC_APP , KC_Z  , KC_B  , KC_M  , KC_G  , KC_W  ,                         KC_X  , KC_DOT,KC_MINS,KC_COMM, KC_Q ,KC_APP,
                        KC_OLCTL,KC_OLALT,                                                      KC_ORALT,KC_ORCTL,
-                                       KC_OLSFT,OSL(_LOWER),                    KC_SPC ,KC_ORSFT,
-                                       KC_ESC  ,KC_RAISO,                      KC_RAISE,OSL(_LOWER),
-                                       KC_OLGUI,                                        KC_ORGUI
+                                       KC_OLSFT,OSL(_LOWER),                   KC_SPC,KC_ORSFT,
+                                       KC_ESC  ,KC_RAISO,                      KC_RAISE   ,OSL(_LOWER),
+                                       KC_OLGUI,                                           KC_ORGUI
     ),
 
-    [_QWERTY] = LAYOUT_5x6(
-        KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_EQL ,
-        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_RBRC,
-        KC_LBRC, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-        KC_APP , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,_______,
-                       KC_OLCTL,KC_OLALT,                                                      KC_ORALT,KC_ORCTL,
-                                       KC_OLSFT,OSL(_LOWER),                    KC_SPC ,KC_ORSFT,
-                                        KC_ESC ,KC_RAISE,                      KC_RAISE,OSL(_LOWER),
-                                       KC_OLGUI,                                        KC_ORGUI
-    ),
 
     [_LOWER] = LAYOUT_5x6(
         _______,KC_F11 ,KC_F12 ,KC_F13 ,KC_F14 ,KC_F15 ,                  KC_F16 ,KC_F17 ,KC_F18 ,KC_F19 ,KC_F20 ,_______,
@@ -65,19 +54,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,KC_CIRC,KC_RBRC,KC_RCBR,KC_RPRN,KC_DLR,                   KC_0   ,KC_1   , KC_2  , KC_3  ,KC_EQL ,_______,
                         _______,_______,                                                  _______,_______,
                                         _______,_______,                  KC_ENT ,_______,
-                                        KC_TAB ,_______,                  _______,_______,
+                                        _______,_______,                  _______,_______,
                                         _______,                                  _______
     ),
 
     [_RAISE] = LAYOUT_5x6(
-        QK_BOOT,_______,_______  ,_______,_______ ,_______,                  KC_QWER,KC_MAG  ,_______,RGB_VAI,RGB_SAI,QK_BOOT,
-        _______,KC_BSPC,C(KC_BSPC),C(KC_DEL),KC_DEL,KC_ESC,                  KC_BSLS, DE_UE  , DE_OE ,_______,_______,RGB_TOG,
-        _______,KC_LEFT,KC_DOWN  ,KC_UP  ,KC_RIGHT,KC_TAB ,                  _______, DE_AE  ,DE_EURO,KC_EXLM,KC_QUES,RGB_MOD,
-        _______,KC_HOME,KC_PGDN,KC_PGUP,KC_END  ,KC_INS ,                  _______,KC_COLN ,KC_TILD,KC_SCLN,DE_SS  ,RGB_HUI,
+        QK_BOOT,_______   ,_______  ,_______  ,KC_CAPS  ,_______,                    KC_QWER,KC_MAG  ,_______,RGB_VAI,RGB_SAI,QK_BOOT,
+        _______,C(KC_BSPC),KC_PSTE  ,_______  ,KC_COPY  ,KC_FIND,                  KC_BSLS, DE_UE  , DE_OE ,_______,_______,RGB_TOG,
+        _______,KC_LEFT   ,KC_DOWN  ,KC_UP    ,KC_RIGHT ,KC_CUT ,                  _______, DE_AE  ,DE_EURO,KC_EXLM,KC_QUES,RGB_MOD,
+        _______,KC_HOME   ,KC_PGDN  ,KC_PGUP  ,KC_BSPC  ,KC_END ,                  KC_DEL ,KC_COLN ,KC_TILD,KC_SCLN,DE_SS  ,RGB_HUI,
                         _______  ,_______,                                                    _______,_______,
-                                          _______ ,_______,                  _______,_______,
+                                          _______ ,_______,                  KC_TAB ,_______,
                                           _______ ,_______,                  _______,_______,
                                           _______ ,                                  _______
+    ),
+
+    [_QWERTY] = LAYOUT_5x6(
+        KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_EQL ,
+        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_RBRC,
+        KC_LBRC, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+        KC_APP , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,_______,
+                       KC_OLCTL,KC_OLALT,                                                      KC_ORALT,KC_ORCTL,
+                                       KC_OLSFT,OSL(_LOWER),                   OSL(_LOWER) ,KC_ORSFT,
+                                        KC_ESC ,KC_RAISE,                      KC_RAISE    ,KC_SPC,
+                                       KC_OLGUI,                                        KC_ORGUI
     ),
 
 /*
